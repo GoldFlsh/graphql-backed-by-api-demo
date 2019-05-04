350da623482ba6bee936421052106c8b4b8eadcc
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2019-05-04 08:10:52 -0500

    Initial commit of spring intiializr project

---
+++ src/main/java/org/ryank/demo/GraphqlBackedByApiDemoApplication.java
@@ -0,0 +1,13 @@
+package org.ryank.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlBackedByApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlBackedByApiDemoApplication.class, args);
+	}
+
+}
