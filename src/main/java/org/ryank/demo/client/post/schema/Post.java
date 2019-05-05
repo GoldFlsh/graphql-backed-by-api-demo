d56da7801d00c0f9f5d06d8326449d6766b51c96
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2019-05-05 00:23:27 -0500

    Add annotations to Post schema

--- src/main/java/org/ryank/demo/client/post/schema/Post.java
+++ src/main/java/org/ryank/demo/client/post/schema/Post.java
@@ -1,8 +1,16 @@
 package org.ryank.demo.client.post.schema;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor(onConstructor = @__(@JsonCreator))
 public class Post {
 
-  Integer id;
-  String title;
-  String body;
+  private Integer id;
+  private String title;
+  private String body;
 }
