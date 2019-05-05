b083d0628fecc14ede5b70bd9abbabd80a814b90
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2019-05-05 09:18:30 -0500

    Use address during mutation, to showcase mutating with an object instead of a simple type

--- src/main/java/org/ryank/demo/graphql/service/UsersClient.java
+++ src/main/java/org/ryank/demo/graphql/service/UsersClient.java
@@ -5,6 +5,7 @@ import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+import org.ryank.demo.graphql.schema.Address;
 import org.ryank.demo.graphql.schema.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,8 +31,9 @@ public class UsersClient {
 
   @GraphQLMutation
   public User updateUser(@GraphQLNonNull @GraphQLArgument(name = "id") Integer id,
-      @GraphQLNonNull @GraphQLArgument(name = "username") String username) {
-    User user = getUser(id).withUsername(username);
+      @GraphQLNonNull @GraphQLArgument(name = "username") String username,
+      @GraphQLNonNull @GraphQLArgument(name = "address")Address address) {
+    User user = getUser(id).withUsername(username).withAddress(address);
     return restTemplate.postForEntity("/users", user, User.class).getBody();
   }
 }
